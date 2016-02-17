4949045331657ab124d8c6465a217486fe2f9273
atefwahab <atefwahab@gmail.com>
2016-02-18 00:43:50 +0200

    Db is created
    Db

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import model.DbConnector;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * @author Atef.
  * Servlet implementation class Controller
  */
 @WebServlet("/Controller")
@@ -26,8 +28,8 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		DbConnector dbConnector=new DbConnector();
+		dbConnector.selectAll();
 	}
 
 	/**
--- src/model/DbConnector.java
+++ src/model/DbConnector.java
@@ -1,5 +1,76 @@
 package model;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Driver;
+
+
+/**
+ * 
+ * @author Atef
+ * this class used to connect to database.
+ *
+ */
 public class DbConnector {
 
+	 /*
+    	Database Connection variables
+	  */
+	Connection con;
+	String driver="com.mysql.jdbc.Driver";
+	String dbUrl="jdbc:mysql://localhost:3306/ecommerce";
+	String dbUserName="root";
+	String pass="";
+	Statement state;
+	String query;
+	ResultSet resultSet;
+	
+	
+	/**
+	 * @author atef
+	 * this is the constructor.
+	 */
+	public DbConnector(){
+		try {
+			DriverManager.registerDriver(new Driver());
+			con=DriverManager.getConnection(dbUrl,dbUserName,pass);
+            
+            //create Statement
+            state=con.createStatement();
+            
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	// this is a trial method
+	public void selectAll(){
+		
+		String query="SELECT * FROM `users`";
+		try {
+			resultSet =state.executeQuery(query);
+			// to print output
+			while(resultSet.next()){
+				for(int i=0;i<resultSet.getMetaData().getColumnCount();i++){
+						
+					System.out.println(resultSet.getString(i));
+				}
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+// end of DB Class.	
 }
